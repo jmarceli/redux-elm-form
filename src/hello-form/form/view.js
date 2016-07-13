29edecc6162791361df2043304c51c82e512bd0e
Jan Grzegorowski <grzegorowski@gmail.com>
2016-07-13 15:30:05 +0200

    feat: improve redux-form example by adding Component based Field

--- src/hello-form/form/view.js
+++ src/hello-form/form/view.js
@@ -1,6 +1,35 @@
 import React from 'react';
 import { reduxForm, Field } from 'redux-form';
 
+/**
+ * Example of Component based field
+ */
+class HideableInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { nameHidden: false };
+    this.toggleName = this.toggleName.bind(this);
+  }
+
+  toggleName() {
+    this.setState({ nameHidden: !this.state.nameHidden });
+  }
+
+  render() {
+    const { input: { visited } } = this.props;
+    return(
+      <div>
+        First Name ({visited? 'visited' : 'not visited'})
+        {this.state.nameHidden
+        ? <input type="password" {...this.props}/>
+        : <input type="text" {...this.props}/>
+        }
+        <label><input type="checkbox" onChange={this.toggleName} /> Hide name</label>
+      </div>
+    );
+  }
+}
+
 // Form as stateless function
 // it can't be "const" because of later reduxForm() binding
 let Form = (props) => {
@@ -11,12 +40,7 @@ let Form = (props) => {
       <legend>Redux-form</legend>
       {submitFailed? <div><strong>Submitting failed</strong></div> : ''}
       Is submitting: <strong>{ submitting? 'Yes' : 'No' }</strong>
-        <Field name="firstName" component={firstName =>
-          <div>
-            First Name ({firstName.visited? 'visited' : 'not visited'})
-            <input type="text" {...firstName}/>
-          </div>
-        }/>
+        <Field name="firstName" component={HideableInput} />
         <Field name="lastName" component={lastName =>
           <div>
             Last Name <input type="text" {...lastName}/>
